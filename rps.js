bbafcd77bc21797610f4ed16ce53613e539571a0
Khizar <syedkhizarazam@gmail.com>
2022-09-14 17:12:24 +0500

    Computer random choice done

--- rps.js
+++ rps.js
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+let compOptions = ["Rock", "Paper", "Scissors"];
+function getComputerChoice(x) {
+  return compOptions[Math.floor(Math.random() * compOptions.length)];
+}
+
+console.log(getComputerChoice());
